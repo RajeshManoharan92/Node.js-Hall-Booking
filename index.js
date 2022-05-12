a08629eba40a484d9ef47f8ea1e376ca001df165
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2022-05-12 15:38:51 +0530

    cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const mongo = require('./shared');
 const BookingRouter = require('./router/hall-booking');
+const cors = require('cors');
 
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors())
 
 mongo.Connect();
 
