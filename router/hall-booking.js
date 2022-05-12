9e57f4ba87cf032e008dfd054c3e9439acfbc30a
RajeshManoharan92 <m.rajeshmanohar@gmail.com>
2022-05-12 16:13:46 +0530

    hall-booking

--- router/hall-booking.js
+++ router/hall-booking.js
@@ -7,9 +7,9 @@ const BookingModule = require('../module/hall-booking');
 router.get('/getroomdetails', BookingModule.getroomdetails);
 router.get('/getcustomerdata', BookingModule.getcustomerdata);
 router.get('/getbookingstatus', BookingModule.getbookingstatus);
-router.post('/postcustomerdetails', BookingModule.postcustomerdetails)
-router.put('/updatecustomerdetails/:id', BookingModule.updatecustomerdetails)
-router.delete('/deletecustomerdetails/:id', BookingModule.deletecustomerdetails)
+router.post('/postcustomerdetails', BookingModule.postcustomerdetails);
+router.put('/updatecustomerdetails/:id', BookingModule.updatecustomerdetails);
+router.delete('/deletecustomerdetails/:id', BookingModule.deletecustomerdetails);
 
 
 module.exports = router;
